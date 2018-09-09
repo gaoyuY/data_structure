a6a0fd061a47168c6e630bd6cbbe74d2d765c810
gaoyuY <2282940822@qq.com>
2018-09-09 20:42:22 +0800

    update

--- Sequence_table_operation.c
+++ Sequence_table_operation.c
@@ -1,12 +1,12 @@
 /*************************************************************************
-> File Name: Sequence_table_operation.c
+> File Name: lianxi.c
 > Author:gaoyu
 > Mail:2282940822 
-> Created Time: 2018年09月09日 星期日 13时27分00秒
+> Created Time: 2018年09月09日 星期日 20时28分56秒
 ************************************************************************/
 
-/***************************************************顺序表的查找，删除，遍历************************************************************/
-#include<stdio.h>
+/***************************************************顺序表的插入，删除，查找，遍历 ******************************************************/
+#include <stdio.h>
 #include <stdlib.h>
 
 #define ERROR 0
@@ -18,19 +18,19 @@ typedef struct Vector {
 
 } Vector;
 
-void init(Vector *vector, int size) { //初始化
-    vector->size = size;
-    vector->length = 0;
-    vector->data = (int *)malloc(sizeof(int) * size);
+void init(Vector *vector, int size) {//初始化
+    vector -> size = size;
+    vector -> length = 0;
+    vector -> data = (int *)malloc(sizeof(int) * size);
 
 }
 
-void expand(Vector *vector) { //扩容
-    int *old_data = vector->data;
-    vector->size = vector->size * 2;
-    vector->data = (int *)malloc(sizeof(int) * vector->size);
-    for (int i = 0; i < vector->length; ++i) {
-        vector->data[i] = old_data[i];
+void expand(Vector *vector) {//扩容
+    int *old_data = vector -> data;
+    vector -> size = vector -> size * 2;
+    vector -> data = (int *)malloc(sizeof(int) * vector -> size);
+    for (int i = 0; i < vector -> length; i++) {
+        vector -> data[i] = old_data[i];
 
     }
     free(old_data);
@@ -38,55 +38,54 @@ void expand(Vector *vector) { //扩容
 }
 
 int insert(Vector *vector, int loc, int value) {//插入
-    if (loc < 0 || loc > vector->length) {
-        return ERROR;
+    if (loc < 0 || loc > vector -> length) {
+        return 0;
 
     }
-    if (vector->length >= vector->size) {
-        //return ERROR;
+    if (vector -> length >= vector -> size) {
         expand(vector);
 
     }
-    for (int i = vector->length; i > loc; --i) {
-        vector->data[i] = vector->data[i - 1];
+    for (int i = vector -> length; i > loc; i--) {
+        vector -> data[i] = vector -> data[i - 1];
 
     }
-    vector->data[loc] = value;
-    vector->length++;
-    return OK;
+    vector -> data[loc] = value;
+    vector -> length ++;
+    return 1; 
 
 }
 
 int search(Vector *vector, int value) {//查找
-    for (int i = 0; i < vector->length; ++i) {
-        if (vector->data[i] == value) {
-            return i;
+    for (int i = 0; i < vector -> length; i++) {
+        if (vector -> data[i] == value) {
+            return 1;
 
         }
 
     }
-    return -1;
+    return 0;
 
 }
 
-int delete_node(Vector *vector, int index) {//删除节点
-    if (index < 0 || index >= vector->length) {
-        return ERROR;
+int delete_node(Vector *vector, int index) {//删除
+    if (index < 0 || index >= vector -> length) {
+        return 0;
 
     }
-    for (int i = index + 1; i < vector->length; ++i) {
-        vector->data[i - 1] = vector->data[i];
+    for (int i = index + 1; i < vector -> length; i++) {
+        vector -> data[i - 1] = vector -> data[i];
 
     }
-    vector->length = vector->length - 1;
-    return OK;
+    vector ->length = vector -> length - 1;
+    return 1;
 
 }
 
-void print(Vector *vector) {//输出
+void print(Vector *vector) {//遍历
     for (int i = 0; i < vector -> length; i++) {
         if (i > 0) {
-            printf (" ");
+            printf(" ");
 
         }
         printf ("%d", vector -> data[i]);
@@ -97,22 +96,58 @@ void print(Vector *vector) {//输出
 }
 
 void clear(Vector *vector) {//释放空间
-    free(vector->data);
+    free(vector -> data);
     free(vector);
 
 }
 
 int main() {
     Vector *a = (Vector *)malloc(sizeof(Vector));
-    init(a, 100);
-    printf("%d\n", insert(a, 0, 1));
-    printf("%d\n", insert(a, 0, 2));
-    print(a);
-    printf("%d\n", delete_node(a, 1));
-    print(a);
-    printf("%d\n", search(a, 0));
-    printf("%d\n", search(a, 1));
-    clear(a);
+    init(a, 20);
+    int m;
+    scanf ("%d", &m);
+    while(m--) {
+        int t, c, b;
+        scanf ("%d", &t);
+        if (t == 1) {//插入
+            scanf("%d%d", &c, &b);
+            if (insert(a, c, b) == 0) {
+                printf ("failed\n");
+
+            }else {
+                printf ("success\n");
+
+            }
+
+        }else if (t == 2) {//删除
+            scanf ("%d", &c);
+            if (delete_node(a, c) == 0) {
+                printf ("failed\n");
+
+            }else {
+                printf ("success\n");
+
+            }
+
+        }else if (t == 3) {//查找
+            scanf ("%d", &c);
+            if (search(a, c) == 0) {
+                printf ("failed\n");
+
+            }else {
+                printf ("success\n");
+
+            }
+
+        }else {//遍历
+            print(a);
+
+
+        }
+
+    }
+
     return 0;
 
 }
+
